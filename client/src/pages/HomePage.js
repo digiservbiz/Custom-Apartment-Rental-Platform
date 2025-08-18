aa4af711e48b0b3b482a2bc46a29d6ddc73e6f04
digiservbiz <digiservbiz@gmail.com>
2025-08-18 11:55:24 +0200

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Find Your Perfect Rental</h1>
+        <h1 className="display-5 fw-bold">{t('home_title')}</h1>
         <p className="col-md-8 fs-4">
-          The best apartments in the city, just a few clicks away.
+          {t('home_subtitle')}
         </p>
         <Link to="/apartments">
           <button className="btn btn-primary btn-lg" type="button">
-            Browse Apartments
+            {t('browse_apartments')}
           </button>
         </Link>
       </div>
