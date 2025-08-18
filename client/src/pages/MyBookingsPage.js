3608352fa0eda28c006f298ccd6e826647826ba3
digiservbiz <digiservbiz@gmail.com>
2025-08-18 11:51:36 +0200

    Update MyBookingsPage.js

--- client/src/pages/MyBookingsPage.js
+++ client/src/pages/MyBookingsPage.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 import Spinner from '../components/Spinner';
 import Alert from '../components/Alert';
+import { useTranslation } from 'react-i18next';
 
 const MyBookingsPage = () => {
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const { user } = useContext(AuthContext);
+  const { t } = useTranslation();
 
   useEffect(() => {
     const fetchMyBookings = async () => {
@@ -46,21 +48,21 @@ const MyBookingsPage = () => {
 
   return (
     <div>
-      <h1>My Bookings</h1>
+      <h1>{t('my_bookings')}</h1>
       {bookings.length === 0 ? (
-        <p>You have no bookings.</p>
+        <p>{t('you_have_no_bookings')}</p>
       ) : (
         bookings.map((booking) => (
           <div key={booking._id} className="card mb-3">
             <div className="card-body">
-                <h5 className="card-title">Apartment: {booking.apartment.location}</h5>
-                <p className="card-text">Check-in: {new Date(booking.checkInDate).toLocaleDateString()}</p>
-                <p className="card-text">Check-out: {new Date(booking.checkOutDate).toLocaleDateString()}</p>
-                <p className="card-text">Total Price: ${booking.totalPrice}</p>
-                <p className="card-text">Status: {booking.status}</p>
+                <h5 className="card-title">{t('apartments')}: {booking.apartment.location}</h5>
+                <p className="card-text">{t('check_in_date')}: {new Date(booking.checkInDate).toLocaleDateString()}</p>
+                <p className="card-text">{t('check_out_date')}: {new Date(booking.checkOutDate).toLocaleDateString()}</p>
+                <p className="card-text">{t('total_price')}: ${booking.totalPrice}</p>
+                <p className="card-text">{t('status')}: {booking.status}</p>
                 {booking.status === 'Confirmed' && (
                 <Link to={`/review/${booking.apartment._id}`} className="btn btn-primary">
-                    Leave a Review
+                    {t('leave_a_review')}
                 </Link>
                 )}
             </div>
